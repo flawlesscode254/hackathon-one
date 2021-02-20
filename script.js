8c8cbb46f0a271097c8e206cb878b68c60310f1f
flawlesscode254 <denzme414@gmail.com>
2021-02-20 21:19:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let a = new Audio('one.mp3')
+let b = new Audio('two.mp3')
+let c = new Audio('three.mp3')
+let d = new Audio('four.mp3')
+let e = new Audio('five.mp3')
+let f = new Audio('six.mp3')
+let g = new Audio('seven.mp3')
+let h = new Audio('eight.mp3')
+let i = new Audio('nine.mp3')
+let j = new Audio('ten.mp3')
+let k = new Audio('eleven.mp3')
+let l = new Audio('twelve.mp3')
+
+function Play1(){
+    a.play();
+}
+function Play2(){
+    b.play();
+}
+function Play3(){
+    c.play();
+}
+function Play4(){
+    d.play();
+}
+function Play5(){
+    e.play();
+}
+function Play6(){
+    f.play();
+}
+function Play7(){
+    g.play();
+}
+function Play8(){
+    h.play();
+}
+function Play9(){
+    i.play();
+}
+function Play10(){
+    j.play();
+}
+function Play11(){
+    k.play();
+}
+function Play12(){
+    l.play();
+}
